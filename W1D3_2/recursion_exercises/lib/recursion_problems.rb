78327f70cc98ed75a3fd43a3ae081394b04bcde3
jyschwrtz <jyschwrtz@gmail.com>
2017-10-07 13:12:42 -0700

    Deleting repository within repository

---
+++ W1D3_2/recursion_exercises/lib/recursion_problems.rb
@@ -0,0 +1,56 @@
+#Problem 1: You have array of integers. Write a recursive solution to find the
+#sum of the integers.
+
+def sum_recur(array)
+  return 0 if array.empty?
+  return array.first if array.length == 1
+  array.first + sum_recur(array[1..-1])
+end
+
+#Problem 2: You have array of integers. Write a recursive solution to determine
+#whether or not the array contains a specific value.
+
+def includes?(array, target)
+  return false if array.empty?
+  return true if array.first == target
+  includes?(array[1..-1], target)
+end
+
+# Problem 3: You have an unsorted array of integers. Write a recursive solution
+# to count the number of occurrences of a specific value.
+
+def num_occur(array, target)
+  return 0 if array.length == 0
+  count = 0
+  if array.first == target
+    count += 1
+  end
+  count + num_occur(array[1..-1], target)
+end
+
+# Problem 4: You have array of integers. Write a recursive solution to determine
+# whether or not two adjacent elements of the array add to 12.
+
+def add_to_twelve?(array)
+  return false if array.length == 1
+  return true if array[0] + array[1] == 12
+  add_to_twelve?(array[1..-1])
+end
+
+# Problem 5: You have array of integers. Write a recursive solution to determine
+# if the array is sorted.
+
+def sorted?(array)
+  return true if array.length <= 1
+  return false if array[0] > array[1]
+  sorted?(array[1..-1])
+end
+
+# Problem 6: Write a recursive function to reverse a string. Don't use any
+# built-in #reverse methods!
+
+def reverse(string)
+  return "" if string.length < 1
+  return string[0] if string.length == 1
+  string[-1] + reverse(string[0..-2])
+end
