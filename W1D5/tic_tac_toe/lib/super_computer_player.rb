db960e7f180b0c223865a01b9a47c4c521a47bbb
jyschwrtz <jyschwrtz@gmail.com>
2017-10-17 10:01:58 -0700

    Creates TicTacToe Super AI with bug

--- W1D5/tic_tac_toe/lib/super_computer_player.rb
+++ W1D5/tic_tac_toe/lib/super_computer_player.rb
@@ -1,13 +1,32 @@
 require_relative 'tic_tac_toe_node'
+require 'byebug'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+    winning_nodes = []
+    non_losing_nodes = []
+    node.children.each do |child_node|
+      if child_node.winning_node?(mark)
+        winning_nodes << child_node
+      else
+        non_losing_nodes << child_node unless child_node.losing_node?(mark)
+      end
+      debugger
+    end
+    #PROBLEM IN HERE -- NOT RETURNING ANYTHING INTO WINNERS OR NON-LOSERS
+    if winning_nodes.empty?
+      raise if non_losing_nodes.empty?
+      non_losing_nodes.first.prev_move_pos
+    else
+      winning_nodes.first.prev_move_pos
+    end
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
   puts "Play the brilliant computer!"
-  hp = HumanPlayer.new("Jeff")
+  hp = HumanPlayer.new("Jay")
   cp = SuperComputerPlayer.new
 
   TicTacToe.new(hp, cp).run
--- W1D5/tic_tac_toe/lib/tic_tac_toe_node.rb
+++ W1D5/tic_tac_toe/lib/tic_tac_toe_node.rb
@@ -1,6 +1,7 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
   def initialize(board, next_mover_mark, prev_move_pos = nil)
     @board = board
     @next_mover_mark = next_mover_mark
@@ -8,14 +9,53 @@ class TicTacToeNode
   end
 
   def losing_node?(evaluator)
-    
+    if @board.over?
+      if @board.winner != evaluator
+        return true
+      else #draw or win
+        return false
+      end
+    end
+
+    if evaluator == @next_mover_mark
+      children.all? { |node| node.losing_node?(evaluator) }
+    else
+      children.any? { |node| node.losing_node?(evaluator) }
+    end
   end
 
   def winning_node?(evaluator)
+    if @board.over?
+      if @board.winner == evaluator
+        return true
+      else
+        return false
+      end
+    end
+
+    if evaluator == @next_mover_mark
+      children.any? { |node| node.winning_node?(evaluator) }
+    else
+      children.all? { |node| node.winning_node?(evaluator) }
+    end
+
   end
 
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    nodes = []
+    @board.rows.each_index do |row|
+      @board.rows[row].each_index do |col|
+        pos = [row, col]
+        if @board.empty?(pos)
+          new_board = @board.dup
+          new_board[pos] = @next_mover_mark
+          next_mark = (@next_mover_mark == :x ? :o : :x )
+          nodes << TicTacToeNode.new(new_board, next_mark, pos)
+        end
+      end
+    end
+    nodes
   end
 end
