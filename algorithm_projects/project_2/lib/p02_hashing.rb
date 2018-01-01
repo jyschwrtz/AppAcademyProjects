e0a33b37ad8123735a315ffc01d6e0db5447003b
jyschwrtz <jyschwrtz@gmail.com>
2018-01-01 13:32:16 -0800

    complete phase 2 hash map - hashing

--- algorithm_projects/project_2/lib/p02_hashing.rb
+++ algorithm_projects/project_2/lib/p02_hashing.rb
@@ -4,11 +4,21 @@ end
 
 class Array
   def hash
+    total = 0
+    self.each_with_index do |val, idx|
+      total += (val * (idx + 1)).hash
+    end
+    total
   end
 end
 
 class String
   def hash
+    total = 0
+    self.chars.each.with_index do |letter, idx|
+      total += (letter.ord * (idx + 1)).hash
+    end
+    total
   end
 end
 
@@ -16,6 +26,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    total = 0
+    self.keys.sort.each.with_index do |key, idx|
+      total += (key.hash + idx.hash + self[key].hash)
+    end
+    total
   end
 end
