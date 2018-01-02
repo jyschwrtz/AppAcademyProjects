669c2ff8073b842bd0c596511444bbdc5e831194
jyschwrtz <jyschwrtz@gmail.com>
2018-01-02 12:20:32 -0800

    dynamic arrays complete without shift/unshift

--- algorithm_projects/project_1/lib/dynamic_array.rb
+++ algorithm_projects/project_1/lib/dynamic_array.rb
@@ -5,42 +5,47 @@ class DynamicArray
 
   def initialize
     @length = 0
-    @capacity = 4
-    @arr = StaticArray.new(@capacity)
+    @capacity = 8
+    @start_pos = 0
+    @store = StaticArray.new(@capacity)
   end
 
   # O(1)
   def [](index)
     check_index(index)
-    @arr[index]
+    @store[index]
   end
 
   # O(1)
   def []=(index, value)
-    check_index(index)
-    @arr[index] = value
+    @store[index] = value
   end
 
   # O(1)
   def pop
+    raise "index out of bounds" if length < 1
     @length -= 1
-
   end
 
   # O(1) ammortized; O(n) worst case. Variable because of the possible
   # resize.
   def push(val)
     @length += 1
+    resize! if @length > @capacity
+    self[@length] = val
   end
 
   # O(n): has to shift over all the elements.
   def shift
+    raise "index out of bounds" if length < 1
     @length -= 1
   end
 
   # O(n): has to shift over all the elements.
   def unshift(val)
     @length += 1
+    resize! if @length > @capacity
+    self[@length] = val
   end
 
   protected
@@ -48,15 +53,14 @@ class DynamicArray
   attr_writer :length
 
   def check_index(index)
-    raise "index out of bounds" unless @arr[index]
-    if @arr.length
+    raise "index out of bounds" unless index < @length
   end
 
   # O(n): has to copy over all the elements to the new store.
   def resize!
     @capacity *= 2
-    @old_arr = @arr.dup
-    @arr = StaticArray.new(@capacity)
-    
+    @old_store = @store.dup
+    @store = StaticArray.new(@capacity)
+
   end
 end
--- algorithm_projects/project_1/lib/static_array.rb
+++ algorithm_projects/project_1/lib/static_array.rb
@@ -1,19 +1,18 @@
 # This class just dumbs down a regular Array to be statically sized.
 class StaticArray
   def initialize(length)
-    @arr = Array.new(length)
+    @store = Array.new(length)
   end
 
   # O(1)
   def [](index)
-    raise "index out of bounds" unless @arr[index]
-    @arr[index]
+    @store[index]
   end
 
   # O(1)
   def []=(index, value)
-    raise "index out of bounds" unless @arr[index]
-    @arr[index] = value
+    @store[index] = value
+
   end
 
   protected
