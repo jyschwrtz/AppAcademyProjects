e8f53a4f72b02f3cf9705e24ec6eb2f69edbbefb
jyschwrtz <jyschwrtz@gmail.com>
2018-01-02 13:57:32 -0800

    began queuewithmax

--- algorithm_projects/project_1/lib/queue_with_max.rb
+++ algorithm_projects/project_1/lib/queue_with_max.rb
@@ -12,18 +12,32 @@ class QueueWithMax
   attr_accessor :store
 
   def initialize
+    @store = RingBuffer.new
+    @ordered_store = RingBuffer.new
   end
 
   def enqueue(val)
+    if max == nil || val > max
+      @ordered_store.push(val)
+    end
+    @store.push(val)
+
   end
 
   def dequeue
+    @store.shift
   end
 
   def max
+    if length > 0
+      @ordered_store[length - 1]
+    else
+      nil
+    end
   end
 
   def length
+    @store.length
   end
 
 end
